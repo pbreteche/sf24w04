0b772fa916a53daef781dd201041b45de41e6913
Pierre Bretéché <pbreteche@dawan.fr>
2024-01-25 13:38:52 +0100

    make crud

---
+++ src/Form/CongeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conge;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CongeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type')
+            ->add('debut')
+            ->add('benefciaire', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Conge::class,
+        ]);
+    }
+}
