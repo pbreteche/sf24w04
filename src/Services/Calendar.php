ea083a18cc11435d9d278550b2102844d44d189d
Pierre Bretéché <pbreteche@dawan.fr>
2024-01-26 08:50:16 +0100

    configure services

--- src/Services/Calendar.php
+++ src/Services/Calendar.php
@@ -2,8 +2,17 @@
 
 namespace App\Services;
 
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+
+#[Autoconfigure(lazy: true)]
 readonly class Calendar
 {
+    public function __construct(
+        private \DateTimeZone $timezone
+    ) {
+
+    }
+
     public function isWeekend(\DateTimeInterface|string $date = null): bool
     {
         $date = is_null($date) || is_string($date) ? new \DateTimeImmutable($date) : $date;
